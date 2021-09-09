e8d1f0aee1032254dc4ea94ec6ea783c0218eb7d
oekis <oekisto@gmail.com>
2021-09-09 21:05:39 +0800

    menambah next/link and add rules

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,3 +1,25 @@
+import Link from 'next/link';
+
 export default function index() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <ul>
+        <li>
+          <Link href="/">
+            <a>Home</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/blog">
+            <a>Blog</a>
+          </Link>
+        </li>
+        <li>
+          <Link href="/about">
+            <a>About</a>
+          </Link>
+        </li>
+      </ul>
+    </header>
+  );
 }
---
+++ pages/blog/index.js
@@ -0,0 +1,9 @@
+import Layout from '../../components/Layout';
+
+export default function index() {
+  return (
+    <Layout title="Blog NextJS">
+      <h1>Blog</h1>
+    </Layout>
+  );
+}
