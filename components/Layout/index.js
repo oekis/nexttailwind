42eabb614c8fd2036b4bc23cf0ec46e08c05719d
oekis <oekisto@gmail.com>
2021-09-09 23:16:30 +0800

    Menambah Favicon

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -8,6 +8,16 @@ export default function Layout(props) {
       <Head>
         <title> {props.title} </title>
         <meta name="description" content="NextJS" />
+        <link rel="icon" type="image/x-icon" href="favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=Source+Sans+Pro:wght@400;600&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <Header />
       {props.children}
--- pages/_document.js
+++ pages/_document.js
@@ -3,18 +3,8 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
-        <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link
-            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&display=swap"
-            rel="stylesheet"
-          />
-          <link
-            href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono&family=Source+Sans+Pro:wght@400;600&display=swap"
-            rel="stylesheet"
-          />
-        </Head>
+      <Html lang="id">
+        <Head />
         <body>
           <Main />
           <NextScript />
